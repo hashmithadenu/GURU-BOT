ef3253b5fc0e0b83fe9d9790ba63a9885023e0aa
hashmithadenu <134828678+hashmithadenu@users.noreply.github.com>
2023-06-02 21:03:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['910714895545', 'HASHMITHA', true],
+  ['620706290014', 'Ash', true], 
   [''] 
 ] //Number of owners
 
